ec670f65308fa0040f22277bf83d9a389dadebc3
Michael Josephson <mike@josephson.org>
2016-04-23 16:29:22 +0100

    end of ex1

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('orgs'); // /orgs
+  this.route('org', {path: 'org/:id'}, function() { // /rg/emberjs
+    this.route('repos');
+    this.route('repo', {path: ':repoid'}, function() {
+      this.route('contributors');
+      this.route('issues');
+    }); // org/jquery/jquery-ui
+  });
+  this.route('notfound', {path:'*path'});
 });
 
 export default Router;
