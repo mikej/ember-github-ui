bad51342580e90f53d2ad421fa25fcb570e247a5
Michael Josephson <mike@josephson.org>
2016-04-28 17:12:57 +0100

    rough work on querying GitHub API for repos

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    let orgName = this.modelFor('org').id;
+    return $.get(`https://api.github.com/orgs/${orgName}/repos`).then(raw => {
+      raw.oldId = raw.id;
+      raw.id = raw.name;
+      return raw;
+    });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('org', this.modelFor('org'));
+  }
 });
