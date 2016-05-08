719c5db6f487a9796db153580fc139f9f37c252a
Michael Josephson <mike@josephson.org>
2016-05-08 16:32:40 +0100

    unit tests for isInArray util

--- tests/unit/utils/is-in-array-test.js
+++ tests/unit/utils/is-in-array-test.js
@@ -5,6 +5,23 @@ module('Unit | Utility | is in array');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = isInArray();
-  assert.ok(result);
+  
+  const Example = Ember.Object.extend({
+    item: 6,
+    list: [1, 2, 3],
+    itemIsInList: isInArray('item', 'list')
+  });
+
+  const exampleToTest = Example.create();
+
+  assert.equal(exampleToTest.get('itemIsInList'), false, 'should not be in the list before being added');
+
+  exampleToTest.get('list').addObject(6);
+
+  assert.equal(exampleToTest.get('itemIsInList'), true, 'should be in the list after adding');
+
+  exampleToTest.set('item', 42);
+
+  assert.equal(exampleToTest.get('itemIsInList'), false, 'should not be in the list after item is changed');
+
 });
