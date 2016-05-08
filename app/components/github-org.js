0286df9bfe261e18ca9686c5074b2b640e076fb6
Michael Josephson <mike@josephson.org>
2016-05-08 13:53:23 +0100

    add isFavourited computed property to github-org

--- app/components/github-org.js
+++ app/components/github-org.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
+import isInArray from 'github-ui/utils/is-in-array';
 
 export default Ember.Component.extend({
   tagName: 'li',
+  favourites: Ember.inject.service(),
+  isFavourited: isInArray('org', 'favourites.orgs'),
   actions: {
     favouriteWasClicked() {
       console.log('fave in the component');
